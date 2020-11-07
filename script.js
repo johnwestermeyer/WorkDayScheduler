502a3431c3607b7c8df4a59087e62c1e8e7cdb68
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-11-06 16:53:21 -0800

    fixed color coding to work with single digit ids

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let today = moment().format('MMMM Do YYYY');
 $("#currentDay").text(today);
-let startTime = 9;
+let startTime = 09;
 let endTime = 17;
 
 //timeblock display
@@ -8,7 +8,6 @@ function timeBlock(){
     let i = startTime;
     while(i <= endTime){
         let j = moment(`${i}:00`, `HH:mm`).format(`hh a`);
-        console.log(j);
         $(".container").append(`<div class="row time-block"><div class="col-2 hour">${j}</div><div class="col-8" id="${i}"><textarea></textarea></div><div class="col-2"><button class="saveBTN">Save</button></div></div>`);
         i++;
     }
@@ -18,12 +17,11 @@ function timeBlock(){
 //timeblock color coding (past, present, future)
 function colorCode(){
     let rows = document.querySelectorAll(".col-8");
-    let current = moment().format('HH');
-    console.log(current);
+    let current = parseInt(moment().format('HH'));
     rows.forEach(element => {
-        if(element.id < current){
+        if(parseInt(element.id) < current){
             element.setAttribute("class", "col-8 past");
-        } else if (element.id > current){
+        } else if (parseInt(element.id) > current){
             element.setAttribute("class", "col-8 future");
         } else {
             element.setAttribute("class", "col-8 present")
